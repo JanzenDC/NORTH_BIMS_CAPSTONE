4f7d5988d0b340013e213c7e70ed55fca929b70d
JanzenDC <janzendelacruz28@gmail.com>
2024-10-11 18:29:54 +0800

    initialize repository

---
+++ pages/headers.php
@@ -0,0 +1,11 @@
+<!-- CSS -->
+<!-- TAILWIND CSS -->
+<link href="../src/output.css" rel="stylesheet">
+<link href="../assets/all.css" rel="stylesheet">
+<link href="../assets/toaster.min.css" rel="stylesheet">
+
+
+<!-- JAVASCRIPT -->
+ <script src="../assets/js/all.js"></script>
+ <script src="../assets/js/jquery-3.6.0.js"></script>
+ <script src="../assets/js/toastr.min.js"></script>
